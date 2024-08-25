e2a6104e7578942f5d13777efaa609f3aaa077c5
dhinesh1357 <pottadhinesh2004@gmail.com>
2024-08-25 23:05:51 +0530

    changed url

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 		// const url = `http://localhost:3000/update-password/${token}`
-		const url = `https://studynotion-op6w.onrender.com/update-password/${token}`;
+		const url = `https://studynotion-op6w.onrender.com/${token}`;
 
 		await mailSender(
 			email,
